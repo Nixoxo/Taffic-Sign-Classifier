28fd3d7be02f63e5ce61ccdf12ce2a513b7582d3
nixoxo <nico.axtmann95@gmail.com>
2017-01-26 17:37:20 +0100

    Extracted the lenet model

--- models/lenet.py
+++ models/lenet.py
@@ -16,7 +16,7 @@ def LeNet(x):
 
     # SOLUTION: Pooling. Input = 28x28x6. Output = 14x14x6.
     conv1 = tf.nn.max_pool(conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
-
+    print(conv1)
     # SOLUTION: Layer 2: Convolutional. Output = 10x10x16.
     conv2_W = tf.Variable(tf.truncated_normal(shape=(5, 5, 6, 16), mean = mu, stddev = sigma))
     conv2_b = tf.Variable(tf.zeros(16))
@@ -27,10 +27,10 @@ def LeNet(x):
 
     # SOLUTION: Pooling. Input = 10x10x16. Output = 5x5x16.
     conv2 = tf.nn.max_pool(conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
-
+    print(conv2)
     # SOLUTION: Flatten. Input = 5x5x16. Output = 400.
     fc0   = flatten(conv2)
-
+    print(fc0)
     # SOLUTION: Layer 3: Fully Connected. Input = 400. Output = 120.
     fc1_W = tf.Variable(tf.truncated_normal(shape=(400, 120), mean = mu, stddev = sigma))
     fc1_b = tf.Variable(tf.zeros(120))
@@ -38,7 +38,7 @@ def LeNet(x):
 
     # SOLUTION: Activation.
     fc1    = tf.nn.relu(fc1)
-
+    print(fc1)
     # SOLUTION: Layer 4: Fully Connected. Input = 120. Output = 84.
     fc2_W  = tf.Variable(tf.truncated_normal(shape=(120, 84), mean = mu, stddev = sigma))
     fc2_b  = tf.Variable(tf.zeros(84))
@@ -46,10 +46,10 @@ def LeNet(x):
 
     # SOLUTION: Activation.
     fc2    = tf.nn.relu(fc2)
-
+    print(fc2)
     # SOLUTION: Layer 5: Fully Connected. Input = 84. Output = 10.
     fc3_W  = tf.Variable(tf.truncated_normal(shape=(84, 43), mean = mu, stddev = sigma))
     fc3_b  = tf.Variable(tf.zeros(43))
     logits = tf.matmul(fc2, fc3_W) + fc3_b
-
+    print(logits)
     return logits
